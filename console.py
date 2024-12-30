7b03469da6f976f132f6e4399d732373074e5a21
EugineI <eugineindimuli@gmail.com>
2024-12-30 17:39:08 +0300

    Database created

--- console.py
+++ console.py
@@ -36,7 +36,7 @@ class HBNBCommand(cmd.Cmd):
             print('(hbnb)')
 
     def precmd(self, line):
-        """Reformat command line for advanced command syntax.
+setup_mysql_dev.sql        """Reformat command line for advanced command syntax.
 
         Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
         (Brackets denote optional fields in usage example.)
---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- Create a database, userand give priviledges--
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON perfomance_scheme.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
