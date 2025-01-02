21a2b9b22bfd69bf702a949863f65181fcb9a675
EugineI <eugineindimuli@gmail.com>
2025-01-02 23:08:04 +0300

    task 9

--- models/user.py
+++ models/user.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,4 +11,6 @@ class User(BaseModel, Base):
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable= True)
+    last_name = Column(String(128), nulilable= True)
+
+    reviews = relationship('Review', backref='user', cascade='all, delete,delete-orphan')
